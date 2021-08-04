8db3fb4fced72e920caf39ca37e4cd6b39edbe66
krishum77 <balccbf@gmail.com>
2021-08-04 17:06:09 +0200

    visualizing guessed letter

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ chosen_word = rand.choice(word_list)
 print(chosen_word)
 guess = input('Enter a Letter: ')
 guess.lower()
-if guess in chosen_word:
-    print("You got it!")
-else:
-    print("Try again!")
+
+display = []
+for letter in chosen_word:
+    display.append('_')
+
+print(display)
+# chosen_word_chars = list(set(chosen_word))
+
+for i in range(len(chosen_word)):
+    if guess in chosen_word:
+        letter = chosen_word[i]
+        if letter == guess:
+            display[i] = letter
+    else:
+        print("Try again!")
+print(display)
+
